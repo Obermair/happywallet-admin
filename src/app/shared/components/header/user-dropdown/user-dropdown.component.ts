11e35fe05859448a060d29906d7409af2c298c4f
David Obermair <46714163+Obermair@users.noreply.github.com>
2025-09-15 10:26:02 +0200

    initial commit

---
+++ src/app/shared/components/header/user-dropdown/user-dropdown.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { DropdownComponent } from '../../ui/dropdown/dropdown.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DropdownItemTwoComponent } from '../../ui/dropdown/dropdown-item/dropdown-item.component-two';
+import { AvatarTextComponent } from '../../ui/avatar/avatar-text.component';
+
+@Component({
+  selector: 'app-user-dropdown',
+  templateUrl: './user-dropdown.component.html',
+  imports:[CommonModule,RouterModule,DropdownComponent,DropdownItemTwoComponent, AvatarTextComponent]
+})
+export class UserDropdownComponent {
+  isOpen = false;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+  }
+}
